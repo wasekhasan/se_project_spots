4890345a1f84965ce80e16a083f7e3d3bef0634d
physics <physicsone7@gmail.com>
2026-04-03 19:53:47 -0400

    stage 5

--- scripts/index.js
+++ scripts/index.js
@@ -68,3 +68,18 @@ function handleAddCardSubmit(evt) {
 }
 
 newPostForm.addEventListener("submit", handleAddCardSubmit);
+
+document.querySelector(".profile__add-button").addEventListener('click', (e) => {
+    document.querySelector("#newPostModal").classList.add('modal_is-opened');
+    //console.log(e)
+
+});
+
+document.querySelector("#cancelNewpost").addEventListener('click', (e) => {
+    document.querySelector("#editProfileModal").classList.remove('modal_is-opened')
+});
+
+document.querySelector("#cancelEditProfile").addEventListener('click', (e) => {
+    document.querySelector("#newPostModal").classList.remove('modal_is-opened')
+});
+
