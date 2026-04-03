be94d976d517c4a9d2056b5a57a54f57ac51f139
physics <physicsone7@gmail.com>
2026-04-03 01:30:58 -0400

    stage 5

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,27 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "input[name='name']"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "input[name='description']"
+);
 
 const newPostButton = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostLinkInput = newPostModal.querySelector("input[name='link']");
+const newPostCaptionInput = newPostModal.querySelector(
+  "input[name='caption']"
+);
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
 
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
@@ -17,12 +34,23 @@ function closeModal(modal) {
 }
 
 editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent;
   openModal(editProfileModal);
 });
 
 editProfileCloseButton.addEventListener("click", function () {
   closeModal(editProfileModal);
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+  closeModal(editProfileModal);
+}
+
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
 
 newPostButton.addEventListener("click", function () {
   openModal(newPostModal);
@@ -31,3 +59,12 @@ newPostButton.addEventListener("click", function () {
 newPostCloseButton.addEventListener("click", function () {
   closeModal(newPostModal);
 });
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log(newPostLinkInput.value);
+  console.log(newPostCaptionInput.value);
+  closeModal(newPostModal);
+}
+
+newPostForm.addEventListener("submit", handleAddCardSubmit);
