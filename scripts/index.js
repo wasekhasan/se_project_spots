552e19fe349f7ee6bdee37de7616e7e4f20493b5
physics <physicsone7@gmail.com>
2026-04-03 01:05:22 -0400

    stage 4

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+
+const newPostButton = document.querySelector(".profile__add-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
+editProfileButton.addEventListener("click", function () {
+  openModal(editProfileModal);
+});
+
+editProfileCloseButton.addEventListener("click", function () {
+  closeModal(editProfileModal);
+});
+
+newPostButton.addEventListener("click", function () {
+  openModal(newPostModal);
+});
+
+newPostCloseButton.addEventListener("click", function () {
+  closeModal(newPostModal);
+});
